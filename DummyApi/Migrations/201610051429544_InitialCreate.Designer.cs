80c655ba3ac9d553ac2abc7b2e1a7b67084ed8ee
John Fristedt <john.fristedt@chas.se>
2016-10-05 16:30:44 +0200

    Redid migrations

--- DummyApi/Migrations/201610051429544_InitialCreate.Designer.cs
+++ DummyApi/Migrations/201610051429544_InitialCreate.Designer.cs
@@ -7,13 +7,13 @@ namespace DummyApi.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class posts_upvote : IMigrationMetadata
+    public sealed partial class InitialCreate : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(posts_upvote));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
         
         string IMigrationMetadata.Id
         {
-            get { return "201610041426139_posts_upvote"; }
+            get { return "201610051429544_InitialCreate"; }
         }
         
         string IMigrationMetadata.Source
---
+++ DummyApi/Migrations/201610051429544_InitialCreate.cs
@@ -0,0 +1,58 @@
+namespace DummyApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.channels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(unicode: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.messages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Author = c.String(unicode: false),
+                        TimeSent = c.DateTime(nullable: false, precision: 0),
+                        Body = c.String(unicode: false),
+                        ChannelId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.channels", t => t.ChannelId, cascadeDelete: true)
+                .Index(t => t.ChannelId);
+            
+            CreateTable(
+                "dbo.posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(unicode: false),
+                        Author = c.String(unicode: false),
+                        PostDate = c.DateTime(nullable: false, precision: 0),
+                        Body = c.String(unicode: false),
+                        Upvotes = c.Int(nullable: false),
+                        Downvotes = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.messages", "ChannelId", "dbo.channels");
+            DropIndex("dbo.messages", new[] { "ChannelId" });
+            DropTable("dbo.posts");
+            DropTable("dbo.messages");
+            DropTable("dbo.channels");
+        }
+    }
+}
